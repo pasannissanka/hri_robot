df2ed842d7c62d12ec975009459bdc70ed5f99a0
pasannissanka <pasankanishkanissanka@gmail.com>
2022-12-21 08:32:55 +0530

    disable facedetection on wake word

--- src/face_detection.py
+++ src/face_detection.py
@@ -23,32 +23,38 @@ class FaceDetection:
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
+        self.enable_detection = False
+
     def detect(self):
-        _, img = self.cap.read()
-        img_size = img.shape
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # Detect the faces
-        faces = np.asarray(self.face_cascade.detectMultiScale(gray, 1.1, 4))
-        region_x = 0
-        region_y = 0
-        if (faces.size != 0):
-            region_bnd_x = img_size[0]/3
-            region_bnd_y = img_size[1]/3
-            x_cored = faces[0][0]
-            y_cored = faces[0][1]
-            if region_bnd_x * 2 < x_cored < region_bnd_x * 3:
-                region_x = 1
-            elif region_bnd_x < x_cored < region_bnd_x * 2:
-                region_x = 2
-            elif 0 < x_cored < region_bnd_x:
-                region_x = 3
-            if 0 < y_cored < region_bnd_y:
-                region_y = 1
-            elif region_bnd_y < y_cored < region_bnd_y * 2:
-                region_y = 2
-            elif region_bnd_y * 2 < y_cored < region_bnd_y * 3:
-                region_y = 3
-        return (faces, (region_x, region_y))
+        if self.enable_detection:
+            _, img = self.cap.read()
+            img_size = img.shape
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Detect the faces
+            faces = np.asarray(
+                self.face_cascade.detectMultiScale(gray, 1.1, 4))
+            region_x = 0
+            region_y = 0
+            if (faces.size != 0):
+                region_bnd_x = img_size[0]/3
+                region_bnd_y = img_size[1]/3
+                x_cored = faces[0][0]
+                y_cored = faces[0][1]
+                if region_bnd_x * 2 < x_cored < region_bnd_x * 3:
+                    region_x = 1
+                elif region_bnd_x < x_cored < region_bnd_x * 2:
+                    region_x = 2
+                elif 0 < x_cored < region_bnd_x:
+                    region_x = 3
+                if 0 < y_cored < region_bnd_y:
+                    region_y = 1
+                elif region_bnd_y < y_cored < region_bnd_y * 2:
+                    region_y = 2
+                elif region_bnd_y * 2 < y_cored < region_bnd_y * 3:
+                    region_y = 3
+            return (faces, (region_x, region_y))
+        else:
+            return ([], (0, 0))
 
     def release(self):
         self.cap.release()
